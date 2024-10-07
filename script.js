ce008a1c6fbdbb7f00269293fc1e5d7df781de4c
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-07 19:51:59 +0200

    Ukončení

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.body.innerHTML = "<p>Josef Novák si vydělal: " + plat + " Kč </p>"
 
 document.body.innerHTML += '12' + '5'+ "<br>"
 document.body.innerHTML += 12 + 5
+
+
+const nazev = "Kniha MOJE"
+const autor = "Halina Pawlovská"
+const cena = 500
+const naSklade = true
+
+
+const kniha = {
+    nazev: "Kniha MOJE",
+    autor: "Halina Pawlovská",
+    cena: 500,
+    naSklade: true,
+}
+
+kniha.autor = "Věnceslav"
+kniha.jazyk = "cestina"
+
+document.body.innerHTML += "<p>Kniha: " + nazev + ", autor: " + autor + "</p>"
+document.body.innerHTML += "<p>Kniha: " + kniha.nazev + ", autor: " + kniha.autor + "</p>"
