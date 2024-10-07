935f4589cc7b86ead9e9894a08d3716ed71ca272
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-07 18:35:33 +0200

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const hodinovka = 300
+const pocetHodin = Number(prompt("Zadej pocet hodina zamestnance", "160"))
+const premie = Number(prompt("Zadej mimořádnou odměnu v Kč"))
+const plat = (hodinovka * pocetHodin) + premie
+
+document.body.innerHTML = "<p>Josef Novák si vydělal: " + plat + " Kč </p>"
+
+document.body.innerHTML += '12' + '5'+ "<br>"
+document.body.innerHTML += 12 + 5
